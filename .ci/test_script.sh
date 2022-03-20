1c21233a0cde521170c7515363ce1b4363d3797e
Vyom-Yadav <jackhammervyom@gmail.com>
2022-03-20 17:56:48 +0530

    Testing closed references action

--- .ci/test_script.sh
+++ .ci/test_script.sh
@@ -3,7 +3,7 @@
 set -e
 LINKED_ISSUES_FORMATTED=/tmp/linked_issues
 cat /dev/null > $LINKED_ISSUES_FORMATTED
-LINKED_ISSUES="37,38"
+LINKED_ISSUES="37,38,39"
 CHECKSTYLE_ISSUE_PREFIX="https:\/\/github.com\/Vyom-Yadav\/actions-test\/issues\/"
 if [ ! -z "$LINKED_ISSUES" ]; then
   echo $LINKED_ISSUES | sed -e 's/,/\n/g' >> $LINKED_ISSUES_FORMATTED
