34d61d537c6530765b69004ad70c7d84f1388965
Vyom-Yadav <jackhammervyom@gmail.com>
2022-03-23 08:24:21 +0530

    Temp

--- .ci/test_script.sh
+++ .ci/test_script.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 set -e
+
+# Script requires GITHUB_TOKEN env variable
+MENTIONED_ISSUES=/tmp/mentioned_issues
+CLOSED_ISSUES=/tmp/failed_issues
 LINKED_ISSUES_FORMATTED=/tmp/linked_issues
-cat /dev/null > $LINKED_ISSUES_FORMATTED
-LINKED_ISSUES="37,38,39"
+
+# Linked issues that are mentioned in the code
+LINKED_ISSUES_MENTIONED=/tmp/linked_issues_mentioned
+API_GITHUB_PREFIX="https://api.github.com/repos"
+GITHUB_HOST="https://github.com"
 CHECKSTYLE_ISSUE_PREFIX="https:\/\/github.com\/Vyom-Yadav\/actions-test\/issues\/"
-if [ ! -z "$LINKED_ISSUES" ]; then
-  echo $LINKED_ISSUES | sed -e 's/,/\n/g' >> $LINKED_ISSUES_FORMATTED
-  sed -i "s/^/$CHECKSTYLE_ISSUE_PREFIX/g" $LINKED_ISSUES_FORMATTED
-fi
-cat $LINKED_ISSUES_FORMATTED
+
+cat /dev/null > $MENTIONED_ISSUES
+
+# collect issues where full link is used
+grep -IPonr "(after|[Tt]il[l]?) $GITHUB_HOST/[\w.-]+/[\w.-]+/issues/\d{1,5}" . \
+  | perl -pe 's/:(?!\d).*github.com\//:/' >> $MENTIONED_ISSUES
+
+cat $MENTIONED_ISSUES
--- src/licenseTest2.java
+++ src/licenseTest2.java
@@ -1,3 +1,5 @@
 public class licenseTest2 {
     // Until https://github.com/Vyom-Yadav/actions-test/issues/34
+
+    // Until https://github.com/Vyom/actions-test/issues/34
 }
